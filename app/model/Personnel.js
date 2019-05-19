fae328d721cd2f8fd01802f19ecf606541754920
bojicm <bojic.job@gmail.com>
2019-05-20 00:00:59 +0200

    Store definition

---
+++ app/model/Personnel.js
@@ -0,0 +1,17 @@
+Ext.define('InterviewApp.model.Personnel', {
+    extend: 'Ext.data.Model',
+
+    fields: [
+        { name: 'id', type: 'int' },
+        { name: 'first_name', type: 'string' },
+        { name: 'last_name', type: 'string' },
+        {
+            name: 'fullname', type: 'string', convert: function (val, record) {
+                return record.data.first_name + ' ' + record.data.last_name;
+            }
+        },
+        { name: 'avatar', type: 'string' },
+        { name: 'active', type: 'boolean', defaultValue: 1 }
+
+    ]
+});
